c2f22141bd25eaac6594aac39f8a73421d618b3f
omar abu alhijaa <omaribraheem222@hotmail.com>
2025-08-19 11:48:49 +0300

    finished StaticMembersAndStaticConstructos

---
+++ StaticMembersAndStaticConstructors/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StaticMembersAndStaticConstructors;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     // initialize static member 
+     Console.WriteLine(Student.Count);
+
+     Student.Count = 3;
+     Console.WriteLine(Student.Count);
+    }
+}
+
+/*
+ * Static Members & Static Constructor
+ 
+Goal: track instances.
+
+Add public static int Count and a static ctor to init state.
+
+Accept: Student.Count == 3.
+ */
---
+++ StaticMembersAndStaticConstructors/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace StaticMembersAndStaticConstructors;
+
+public class Student
+{
+    public static int Count { get; set;}
+
+    static Student()
+    {
+        Count = 0;
+    }
+    
+    public Student()
+    {
+        Count++;
+    }
+}
